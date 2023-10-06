37310e8da01ee7646b19b83d0d2335b9555f676b
Digi_WS <asiphe@digilink.africa>
2023-10-06 14:06:18 +0200

    Adde dependencies on the porm file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //System.out.println("Hello world!");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Digi_WS\\Downloads\\chromedriver_win32 (7).zip");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.quit();
     }
+
 }
